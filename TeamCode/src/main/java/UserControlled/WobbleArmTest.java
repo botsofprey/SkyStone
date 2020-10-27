ee443207a244b2e14e347463c4f1bed705c1bd81
botsofprey <BoPest2016>
2020-10-27 18:58:50 -0400

    WobbleArmTest created, though not tested; ran out of time. Hopefully it works!
    - Caiden

---
+++ TeamCode/src/main/java/UserControlled/WobbleArmTest.java
@@ -0,0 +1,130 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package UserControlled;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Wobble Arm Test", group="Testers")
+//@Disabled
+public class WobbleArmTest extends LinearOpMode {
+    // create objects and locally global variables here
+    DcMotor leftMotor, rightMotor, armMotor;
+    double l, r;
+
+    // TODO see how you can instead use encoders to make it only turn to a certain position through use of a limit switch.
+    // run the motor on startup until it reaches the stopping point, then reset the encoders, making your current position the new zero
+
+    @Override
+    public void runOpMode() {
+//        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+//        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+//        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+//        rightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        armMotor = hardwareMap.dcMotor.get("armMotor");
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // initialize objects and variables here
+        // also create and initialize function local variables here
+
+        // add any other useful telemetry data or logging data here
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // nothing goes between the above and below lines
+        waitForStart();
+        // should only be used for a time keeper or other small things, avoid using this space when possible
+        while (opModeIsActive()) {
+            // main code goes here
+
+            if(gamepad1.right_bumper) {
+
+                armMotor.setTargetPosition(0);
+                while (armMotor.isBusy() && opModeIsActive()) {
+                    //Loop body meant to be empty
+                }
+                armMotor.setPower(0);
+
+            }
+
+            if(gamepad1.left_bumper) {
+
+                armMotor.setTargetPosition(-480);
+                while(armMotor.isBusy() && opModeIsActive()) {
+                    //Loop body meant to be empty
+                }
+                armMotor.setPower(0);
+
+            }
+
+            // telemetry and logging data goes here
+//            telemetry.update();
+        }
+        // disable/kill/stop objects here
+    }
+    // misc functions here
+
+    private void tankDrive() {
+        l = 0;
+        r = 0;
+        if(gamepad1.right_trigger > 0.1) r = gamepad1.right_trigger;
+        else if(gamepad1.right_bumper) r = -1;
+        if(gamepad1.left_trigger > 0.1) l = gamepad1.left_trigger;
+        else if(gamepad1.left_bumper) l = -1;
+    }
+
+    private void tankJoystick() {
+        l = -gamepad1.left_stick_y;
+        r = -gamepad1.right_stick_y;
+    }
+
+    private void joystickDrive() {
+        double drive = -gamepad1.left_stick_y;
+        double turn  =  gamepad1.right_stick_x;
+        l    = Range.clip(drive + turn, -1.0, 1.0) ;
+        r   = Range.clip(drive - turn, -1.0, 1.0) ;
+    }
+
+    private void applyMotorPowers(double left, double right) {
+        leftMotor.setPower(left);
+        rightMotor.setPower(right);
+    }
+
+}
