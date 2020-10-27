4749e0c23bc9aee8b5a7ae2df8e44399a372a975
botsofprey <thebotsofprey@gmail.com>
2020-10-27 18:58:11 -0400

    Started intake system class

--- TeamCode/src/main/java/Actions/RingIntakeSystemV1.java
+++ TeamCode/src/main/java/Actions/RingIntakeSystemV1.java
@@ -3,23 +3,15 @@ package Actions;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import MotorControllers.MotorController;
-
 public class RingIntakeSystemV1 {
-
-    MotorController motor;
+    private DcMotor intakeMotor;
 
     public RingIntakeSystemV1(HardwareMap hardwareMap) {
-        motor = new MotorController();
-
+        intakeMotor = hardwareMap.dcMotor.get("intakeMotor");
     }
 
-    public void toggleIntakePower() {
+    public void startIntake(){ intakeMotor.setPower(1); }
+    public void stopIntake(){ intakeMotor.setPower(0); }
 
-    }
-
-    public void toggleIntakeDirection() {
-
-    }
 
 }
