79e9444abd0f928bd44f7c0d9e9878781e5510d4
doesItWorkYet <thebotsofprey@gmail.com>
2020-10-29 18:30:37 -0400

    Wobble Grabber - Needs testing. Test with bare motor first to prevent damaging too much hardware.

---
+++ TeamCode/src/main/java/Actions/WobbleGrabberCaidenTest.java
@@ -0,0 +1,43 @@
+package Actions;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class WobbleGrabberV1 {
+
+    private Servo claw;
+    private DcMotor arm;
+
+    private static final int ARM_GRAB_ANGLE = 45;
+    private static final int ARM_RELEASE_ANGLE = 0;
+    private static final int CLAW_OPEN_ANGLE = 75; //105
+    private static final int CLAW_CLOSE_ANGLE = 105; //75
+
+    public WobbleGrabberV1(HardwareMap hardwareMap) {
+        claw = hardwareMap.servo.get("wobbleGrabberClaw");
+        arm = hardwareMap.dcMotor.get("wobbleGrabberArm");
+        arm.setMode(DcMotor.RunMode.RUN_WITH_ENCODER);
+    }
+
+    public void grabWobbleGoal() {
+        // set position of arm to ARM GRAB ANGLE
+        // grab with the claw
+        arm.setTargetPosition(ARM_GRAB_ANGLE);
+        arm.setPower(.3);
+        claw.setAngle(CLAW_OPEN_ANGLE);
+        while (opModeIsActive() && arm.isBusy()) {idle();}
+        claw.setAngle(CLAW_CLOSE_ANGLE);
+    }
+
+    public void releaseWobbleGoal() {
+        // release with claw
+        // move arm to ARM RELEASE ANGLE
+        arm.setTargetPosition(ARM_RELEASE_ANGLE);
+        arm.setPower(.3);
+        while (opModeIsActive() && leftMotor.isBusy()) {idle();}
+        claw.setAngle(CLAW_OPEN_ANGLE);
+    }
+
+}
+
