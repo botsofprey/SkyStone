64935c8b41c6776d3117474eee754b7f69ef0de0
botsofprey <thebotsofprey@gmail.com>
2020-10-29 18:49:31 -0400

    Finished intake system class

--- TeamCode/src/main/java/Actions/Ultimate/RingIntakeSystemV1.java
+++ TeamCode/src/main/java/Actions/Ultimate/RingIntakeSystemV1.java
@@ -1,17 +1,31 @@
-package Actions;
+package Actions.Ultimate;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class RingIntakeSystemV1 {
     private DcMotor intakeMotor;
+    private static final int MOTOR_OFF_POWER = 0;
+    private static final int MOTOR_ON_POWER = 1;
 
     public RingIntakeSystemV1(HardwareMap hardwareMap) {
         intakeMotor = hardwareMap.dcMotor.get("intakeMotor");
     }
 
-    public void startIntake(){ intakeMotor.setPower(1); }
-    public void stopIntake(){ intakeMotor.setPower(0); }
+    public void toggleIntakePower() {
+        // Turn intake motor on or off
+        if(intakeMotor.getPower() == MOTOR_ON_POWER){
+            intakeMotor.setPower(MOTOR_OFF_POWER);
+        }
+        else {
+            intakeMotor.setPower(MOTOR_ON_POWER);
+        }
+    }
+
+    public void toggleIntakeDirection() {
+        // Reverse direction of intake motor
+        intakeMotor.setPower(-(intakeMotor.getPower()));
+    }
 
 
 }
