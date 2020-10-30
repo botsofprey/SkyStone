47d849bfc980e7c124a2a992cf97797d22fcad27
botsofprey <thebotsofprey@gmail.com>
2020-10-30 18:02:57 -0400

    Update WobbleGrabberCaidenTest.java
    Refused to let me commit, so I decided to copy and paste into GitHub as it's only one file that I worked on.

--- TeamCode/src/main/java/Actions/WobbleGrabberCaidenTest.java
+++ TeamCode/src/main/java/Actions/WobbleGrabberCaidenTest.java
@@ -9,10 +9,15 @@ public class WobbleGrabberCaidenTest {
     private Servo claw;
     private DcMotor arm;
 
-    private static final int ARM_GRAB_ANGLE = 45;
+    private static final int ARM_HOLD_ANGLE = 45;
     private static final int ARM_RELEASE_ANGLE = 0;
     private static final int CLAW_OPEN_ANGLE = 75; //105
     private static final int CLAW_CLOSE_ANGLE = 105; //75
+
+    private static final int UP_POSITION = ARM_HOLD_ANGLE / 180;
+    private static final int DOWN_POSITION = ARM_RELEASE_ANGLE / 180;
+    private static final int OPEN_POSITION = CLAW_OPEN_ANGLE / 180;
+    private static final int CLOSE_POSITION = CLAW_CLOSE_ANGLE / 180;
 
     public WobbleGrabberCaidenTest(HardwareMap hardwareMap) {
         claw = hardwareMap.servo.get("wobbleGrabberClaw");
@@ -21,25 +26,21 @@ public class WobbleGrabberCaidenTest {
     }
 
     public void grabWobbleGoal() {
-        // set position of arm to ARM GRAB ANGLE
-        // grab with the claw
-        arm.setMode(DcMotor.RunMode.RUN_WITH_ENCODER);
-        arm.setTargetPosition(ARM_GRAB_ANGLE);
+        // move arm to down position
+        // move claw to closed position
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setTargetPosition(DOWN_POSITION);
         arm.setPower(.3);
-        claw.setAngle(CLAW_OPEN_ANGLE);
-        while (opModeIsActive() && arm.isBusy()) {idle();}
-        claw.setAngle(CLAW_CLOSE_ANGLE);
+        claw.setPosition(OPEN_POSITION);
+        claw.setPosition(CLOSE_POSITION);
     }
 
     public void releaseWobbleGoal() {
-        // release with claw
-        // move arm to ARM RELEASE ANGLE
-        arm.setMode(DcMotor.RunMode.RUN_WITH_ENCODER);
-        arm.setTargetPosition(ARM_RELEASE_ANGLE);
+        // move arm to down position
+        // move claw to open position
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setTargetPosition(DOWN_POSITION);
         arm.setPower(.3);
-        while (opModeIsActive() && leftMotor.isBusy()) {idle();}
-        claw.setAngle(CLAW_OPEN_ANGLE);
+        claw.setPosition(OPEN_POSITION);
     }
-
 }
-
