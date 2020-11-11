7d711c6d9492af202590cc971c1ee56b76ad0d5e
botsofprey <thebotsofprey@gmail.com>
2020-11-11 16:05:44 -0500

    Added more to WobbleGrabberV1
    Signed-off-by: botsofprey <thebotsofprey@gmail.com>

--- TeamCode/src/main/java/Actions/Ultimate/WobbleGrabberV1.java
+++ TeamCode/src/main/java/Actions/Ultimate/WobbleGrabberV1.java
@@ -16,12 +16,12 @@ public class WobbleGrabberV1 {
 
     private Servo claw;
     private DcMotor arm;
-    private MotorController armC;
+//    private MotorController armC;
 
     private static final double ARM_POWER = 0.2;
 
-    private static final int ARM_GRAB_ANGLE = 45;
-    private static final int ARM_RELEASE_ANGLE = 0;
+    private static final int ARM_DOWN_ANGLE = 45;
+    private static final int ARM_UP_ANGLE = 0;
 
     private static final double CLAW_GRAB_ANGLE = 1.0;
     private static final double CLAW_RELEASE_ANGLE = 0.5;
@@ -37,16 +37,29 @@ public class WobbleGrabberV1 {
 
     // TODO it's okay if after lifting, the motor begins to fall. Just try and get the servo to grab it and the motor to move up
     public void grabWobbleGoal() {
-        arm.setTargetPosition(ARM_GRAB_ANGLE);
+        arm.setTargetPosition(ARM_DOWN_ANGLE);
         arm.setPower(ARM_POWER);
         while (arm.isBusy());
         arm.setPower(0);
+
         claw.setPosition(CLAW_GRAB_ANGLE);
+
+        arm.setTargetPosition(ARM_UP_ANGLE);
+        arm.setPower(-ARM_POWER);
+        while (arm.isBusy());
+        arm.setPower(0);
     }
 
     public void releaseWobbleGoal() {
+
+        arm.setTargetPosition(ARM_DOWN_ANGLE);
+        arm.setPower(ARM_POWER);
+        while (arm.isBusy());
+        arm.setPower(0);
+
         claw.setPosition(CLAW_RELEASE_ANGLE);
-        arm.setTargetPosition(ARM_RELEASE_ANGLE);
+
+        arm.setTargetPosition(ARM_UP_ANGLE);
         arm.setPower(-ARM_POWER);
         while (arm.isBusy());
     }
