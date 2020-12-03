6aaeb1164cd647200c9b71f4d6447950e286018c
botsofprey <thebotsofprey@gmail.com>
2020-12-03 16:15:34 -0500

    removed some debug lines

--- TeamCode/src/main/java/Autonomous/OpModes/UltimateAuto/UltimateV1AutoRed.java
+++ TeamCode/src/main/java/Autonomous/OpModes/UltimateAuto/UltimateV1AutoRed.java
@@ -79,11 +79,6 @@ public class UltimateV1AutoRed extends LinearOpMode {
         if (shouldNotPark(startTime)) robot.driveToLocationOnInitHeading(RING_DETECTION_POINT);
 
         int numRings = robot.detectNumRings();
-
-        //TODO delete this; these are debug lines
-        telemetry.update();
-        robot.stop();
-
         Location ringZone = robot.getZone(numRings);
         telemetry.addData("Rings Found", numRings);
         telemetry.update();
